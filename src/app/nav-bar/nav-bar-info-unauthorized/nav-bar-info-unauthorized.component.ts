e1e3070d8957814fd349980e60638ec8eae538b8
Md Turin <matkhan01@yahoo.com>
2023-05-12 12:25:50 +0600

    login modal added

--- src/app/nav-bar/nav-bar-info-unauthorized/nav-bar-info-unauthorized.component.ts
+++ src/app/nav-bar/nav-bar-info-unauthorized/nav-bar-info-unauthorized.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-nav-bar-info-unauthorized',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar-info-unauthorized.component.css']
 })
 export class NavBarInfoUnauthorizedComponent {
-
+  constructor(public modalService: NgbModal){}
+  
+  openModal(content: any){
+    this.modalService.open(content, {backdrop: 'static', centered: true});
+  }
 }
