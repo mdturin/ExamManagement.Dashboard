0a07db1fbf5f03fe0749264053090d877097f695
Md Turin <matkhan01@yahoo.com>
2023-05-12 12:38:01 +0600

    simple login modal added

--- src/app/nav-bar/nav-bar.module.ts
+++ src/app/nav-bar/nav-bar.module.ts
@@ -4,16 +4,18 @@ import { NavBarComponent } from './nav-bar.component';
 import { NavBarInfoComponent } from './nav-bar-info/nav-bar-info.component';
 import { NavBarInfoAuthorizedComponent } from './nav-bar-info-authorized/nav-bar-info-authorized.component';
 import { NavBarInfoUnauthorizedComponent } from './nav-bar-info-unauthorized/nav-bar-info-unauthorized.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     NavBarComponent,
     NavBarInfoComponent,
     NavBarInfoAuthorizedComponent,
-    NavBarInfoUnauthorizedComponent,
+    NavBarInfoUnauthorizedComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [NavBarComponent]
 })
